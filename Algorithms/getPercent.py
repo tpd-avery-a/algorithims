baafb3f024166d95fbc06de3123f9d2676f89d7a
tpd-avery-a <111819206+tpd-avery-a@users.noreply.github.com>
2022-08-24 13:15:23 -0400

    Add files via upload

---
+++ Algorithms/getPercent.py
@@ -0,0 +1,25 @@
+
+from tempfile import tempdir
+
+
+arr = [19,45,89,63,34]
+
+#get total
+# for each create new array with percent
+def get(arr):
+    total = sum(arr)
+    newArr = []
+    for i in arr:
+        temp = (i/total) * 100
+        newArr.append(temp)
+    print(total)
+    print(newArr)
+
+get(arr)
+
+
+def getAvg(arr):
+    return print(sum(arr)/len(arr))
+
+getAvg(arr)
+
